09f1eb40562c3648d424eed176cc4f9c902535cc
Emma Maddocks <emma.l.maddocks@gmail.com>
2022-10-10 15:58:16 +0100

    this commit adds the models for the get article by ID end point

--- models.js
+++ models.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 const db = require('./db/connection');
 const format = require('pg-format');
+const utils = require('./db/seeds/utils');
 
 exports.getAllTopics = async () => {
     const topics = await db.query(`
@@ -13,7 +14,10 @@ exports.getArticleById = async (article_id) => {
     const article = await db.query(`
     SELECT * FROM articles
     WHERE article_id = $1`, [article_id])
-    return article.rows;
-}
+    if (!article.rows) {
+        await checkExists('articles', 'article_id', article_id); // We, the devs, pass in the table and column name
+        } return article.rows;
+};
+
   
 
