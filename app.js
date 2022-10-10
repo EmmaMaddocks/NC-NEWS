ff02fb0f76e1391def5c9df8ffa41380a4227722
Emma Maddocks <emma.l.maddocks@gmail.com>
2022-10-10 13:42:25 +0100

    this commit adds the error handling middleware functions to the app.js file

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+// const { application } = require('express');
 const express = require('express');
 const app = express();
-const { fetchAllTopics } = require('');
+const { fetchAllTopics } = require('./controllers');
+
 
 app.use(express.json());
 
-app.get('api/topics', fetchAllTopics)
+app.get('/api/topics', fetchAllTopics)
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({ message: err.msg });
+    } else {
+        next(err);
+    }
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: 'server error' });
+})
+
+module.exports = app;
