e98491c9c49150d89c59d1c809cc4f4fda7b53ba
Emma Maddocks <emma.l.maddocks@gmail.com>
2022-10-13 12:10:51 +0100

    this commit adds the post comment feature

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
 // const { application } = require('express');
-const { application } = require('express');
-const express = require('express');
+const { application } = require("express");
+const express = require("express");
 const app = express();
-const { fetchAllTopics, fetchArticleById, fetchUpdatedVotes, fetchAllUsers, fetchAllArticles, fetchComments } = require('./controllers');
-const { getAllArticles } = require('./models');
-
+const {
+  fetchAllTopics,
+  fetchArticleById,
+  fetchUpdatedVotes,
+  fetchAllUsers,
+  fetchAllArticles,
+  fetchComments,
+  postComment,
+} = require("./controllers");
+const { getAllArticles } = require("./models");
 
 app.use(express.json());
 
-app.get('/api/topics', fetchAllTopics)
+app.get("/api/topics", fetchAllTopics);
 
-app.get('/api/articles', fetchAllArticles)
+app.get("/api/articles", fetchAllArticles);
 
-app.get('/api/articles/:article_id', fetchArticleById)
+app.get("/api/articles/:article_id", fetchArticleById);
 
-app.get('/api/users', fetchAllUsers)
+app.get("/api/users", fetchAllUsers);
 
-app.patch('/api/articles/:article_id', fetchUpdatedVotes)
+app.patch("/api/articles/:article_id", fetchUpdatedVotes);
 
-app.get('/api/articles/:article_id/comments', fetchComments)
+app.get("/api/articles/:article_id/comments", fetchComments);
 
+app.post("/api/articles/:article_id/comments", postComment);
 
 app.use((err, req, res, next) => {
-    if (err.code === '22P02') {
-      res.status(400).send({ msg: 'Bad Request' });
-    } else {
-        next(err);
-    }
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
+  }
 });
 
-  app.use((err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).send({ message: err.msg });
-    } else {
-        next(err);
-    }
-})
-
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ message: err.msg });
+  } else {
+    next(err);
+  }
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: 'server error' });
-})
-
-
+  res.status(500).send({ message: "server error" });
+});
 
 module.exports = app;
--- controllers.js
+++ controllers.js
@@ -1,59 +1,77 @@
-const { getAllTopics, getArticleById, getAllUsers, getUpdatedVotes, getAllArticles, getAllComments } = require('./models')
+const {
+  getAllTopics,
+  getArticleById,
+  getAllUsers,
+  getUpdatedVotes,
+  getAllArticles,
+  getAllComments,
+  publishComment,
+} = require("./models");
 
 exports.fetchAllTopics = (req, res, next) => {
-    getAllTopics()
+  getAllTopics()
     .then((topics) => {
-    res.status(200)
-    .send(topics);
+      res.status(200).send(topics);
     })
     .catch(next);
-}
+};
 
 exports.fetchArticleById = (req, res, next) => {
-    const { article_id } = req.params;
-    getArticleById(article_id)
+  const { article_id } = req.params;
+  getArticleById(article_id)
     .then((article) => {
-    res.status(200).send(article[0]);
+      res.status(200).send(article[0]);
     })
     .catch(next);
-}
+};
 
 exports.fetchAllUsers = (req, res, next) => {
-    getAllUsers()
+  getAllUsers()
     .then((users) => {
-    res.status(200)
-    .send(users);
+      res.status(200).send(users);
     })
     .catch(next);
-}
+};
 
 exports.fetchUpdatedVotes = (req, res, next) => {
-    const {inc_votes} = req.body;
-    const { article_id } = req.params;
-    getUpdatedVotes(article_id, inc_votes)
+  const { inc_votes } = req.body;
+  const { article_id } = req.params;
+  getUpdatedVotes(article_id, inc_votes)
     .then((updatedVotes) => {
-    res.status(200).send(updatedVotes)
+      res.status(200).send(updatedVotes);
     })
     .catch(next);
-}
+};
 
 exports.fetchAllArticles = (req, res, next) => {
-    const { topic } = req.query;
+  const { topic } = req.query;
 
-    getAllArticles(topic)
+  getAllArticles(topic)
     .then((result) => {
-    res.status(200).send(result);
+      res.status(200).send(result);
     })
     .catch(next);
-}
-
+};
 
 exports.fetchComments = (req, res, next) => {
-    const { article_id } = req.params;
+  const { article_id } = req.params;
 
-    getAllComments(article_id)
+  getAllComments(article_id)
     .then((comments) => {
-    res.status(200).send(comments);
+      res.status(200).send(comments);
     })
     .catch(next);
-}
+};
+
+exports.postComment = (req, res, next) => {
+  const { article_id } = req.params;
+  const newComment = req.body;
+  getArticleById(article_id)
+    .then(() => {
+      return publishComment(article_id, newComment);
+    })
+    .then((comment) => {
+      res.status(201).send(comment);
+    })
+    .catch(next);
+};
